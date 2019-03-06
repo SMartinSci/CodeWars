6199e8403fc5dab6ac82dd46e3885fa8a84ac4f2
SMartinSci <sm674@cornell.edu>
2019-03-06 08:15:32 -0800

    Modify js script for transportation problem, Ruby was entered by mistake.

--- Javascript/Javascript/transportation_on_vacation.js
+++ Javascript/Javascript/transportation_on_vacation.js
@@ -1,27 +1,12 @@
-# Every day you rent the car, car costs $40
-# If you rent for 7 or more days, you get total - $50
-# Or
-# Rent car for >= 3 days, you get total - $20
-
-# The outcome: total for different days
-
-# total = 40 * days - deal
-# 1. Rent >= 7 days - deal($50)
-# 2. Rent >= 3 days - deal($20)
-#    total = 40 * 4 - 20 = 100
-
-
-def rental_car_cost(day)
-    if day >= 7
-        discount = 50
-        total = 40 * day - discount
-    elsif
-        (day < 7) && (day >= 3)
-
-        discount = 20
-        total = 40 * day - discount
-    else
-        discount = 0
-        total = 40 * day - discount
-    end
-end
+function rentalCarCost(d) {
+    let total;
+    const rate = 40;
+    if (d >= 3 && d < 7) {
+        return total = (d * rate) - 20;
+    } else if (d >= 7) {
+        return total = (d * rate) - 50;
+    } else {
+        return total = d * rate;
+    }
+    return total;
+}
