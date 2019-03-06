6ae709aaddcc8e35e425a93cfa52c3376d3014ea
SMartinSci <sm674@cornell.edu>
2019-03-06 08:19:31 -0800

    Add Ruby solution.

---
+++ Ruby/printer_errors.rb
@@ -0,0 +1,19 @@
+def printer_error(s)
+	
+  
+ # numerator = s.gsub(/[a-m]/, '').length
+  #denominator= s.length
+   #numerator / denominator.to_s
+   
+   "#{s.gsub(/[a-m]/, '').length}/#{s.length}" 
+   
+   
+end
+
+# s = string length
+# error: numerator: total error values, denominator: length of string
+# convert to string
+# a to m is true, 
+# error: count how many times n to z exist in the string
+# input = string
+# #{s.count
