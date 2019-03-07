2c0fbb4d9756739180416346e0ca85e024a20c5d
SMartinSci <sm674@cornell.edu>
2019-03-07 08:52:17 -0800

    Add complete problem statements to solutions.

--- Javascript/Javascript/shortest-word.js
+++ Javascript/Javascript/shortest-word.js
@@ -1,7 +1,5 @@
-//codewars challenge
-
+//Problem:
 //Simple, given a string of words, return the length of the shortest word(s).
-
 //String will never be empty and you do not need to account for different data types.
 
 function findShort(s){
--- Javascript/convert-string-to-camel-case.js
+++ Javascript/convert-string-to-camel-case.js
@@ -1,3 +1,11 @@
+//Problem:
+// Complete the method / function so that it converts dash / underscore delimited words into camel casing.The first word within the output should be capitalized only if the original word was capitalized.
+
+//Examples
+// toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+// toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+
+
 function toCamelCase(str){
   let string = str.replace(/[^A-Z0-9]/ig, " ");
   let splitArr = string.split(' ');
